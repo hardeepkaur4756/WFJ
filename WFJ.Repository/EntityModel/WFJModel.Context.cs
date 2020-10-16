5b2d808b319a30e6375cbbef79c4041f338990f2
Developer <Developer>
2020-10-16 11:00:49 +0530

    commit for add userclient table

--- WFJ.Repository/EntityModel/WFJModel.Context.cs
+++ WFJ.Repository/EntityModel/WFJModel.Context.cs
@@ -129,6 +129,7 @@ namespace WFJ.Repository.EntityModel
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Document> Documents { get; set; }
         public virtual DbSet<ErrorLog> ErrorLogs { get; set; }
+        public virtual DbSet<UserClient> UserClients { get; set; }
     
         public virtual int dt_addtosourcecontrol(string vchSourceSafeINI, string vchProjectName, string vchComment, string vchLoginName, string vchPassword)
         {
