21fbfe6adcc2eaad13736964f2b049f2c9d47814
Kapil Sharma <kapil.kumar.sharma4756@gmail.com>
2021-04-08 13:53:41 +0530

    commit for SMTP HOST changes

--- WFJ.Helper/EmailHelper.cs
+++ WFJ.Helper/EmailHelper.cs
@@ -13,21 +13,42 @@ namespace WFJ.Helper
         public static void SendMail(string toEmail,string subject,string body,bool isReply = false)
         {
             MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient(ConfigurationManager.AppSettings["SMTPHost"]);//"mail.tela.com");
-            mail.From = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
+            SmtpClient SmtpServer = new SmtpClient();
             mail.To.Add(toEmail);
-            mail.Subject = subject;
-            mail.Body = body;
-            mail.IsBodyHtml = true;
             if (isReply)
             {
                 mail.ReplyTo = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
             }
-            SmtpServer.Port = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
-            SmtpServer.UseDefaultCredentials = false;
-            SmtpServer.Credentials = new System.Net.NetworkCredential(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]), Convert.ToString(ConfigurationManager.AppSettings["EmailPassword"]));
-            SmtpServer.EnableSsl = true;
-            SmtpServer.Send(mail);
+            mail.From = new MailAddress("servicerequests@wfjlawfirm.com");
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+            SmtpServer.Host = "clientmailrelay.tela.com";
+            SmtpServer.Port = 25;
+            SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            try
+            {
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+            }
+            //MailMessage mail = new MailMessage();
+            //SmtpClient SmtpServer = new SmtpClient(ConfigurationManager.AppSettings["SMTPHost"]);//"mail.tela.com");
+            //mail.From = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
+            //mail.To.Add(toEmail);
+            //mail.Subject = subject;
+            //mail.Body = body;
+            //mail.IsBodyHtml = true;
+            //if (isReply)
+            //{
+            //    mail.ReplyTo = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
+            //}
+            //SmtpServer.Port = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
+            //SmtpServer.UseDefaultCredentials = false;
+            //SmtpServer.Credentials = new System.Net.NetworkCredential(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]), Convert.ToString(ConfigurationManager.AppSettings["EmailPassword"]));
+            //SmtpServer.EnableSsl = true;
+            //SmtpServer.Send(mail);
         }
     }
 }
