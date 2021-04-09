c19be09abad316580a6e6118802cfec6565d1288
Kapil Sharma <kapil.kumar.sharma4756@gmail.com>
2021-04-09 12:04:48 +0530

    commit for email service fixes

--- WFJ.Helper/EmailHelper.cs
+++ WFJ.Helper/EmailHelper.cs
@@ -33,22 +33,6 @@ namespace WFJ.Helper
             catch (Exception ex)
             {
             }
-            //MailMessage mail = new MailMessage();
-            //SmtpClient SmtpServer = new SmtpClient(ConfigurationManager.AppSettings["SMTPHost"]);//"mail.tela.com");
-            //mail.From = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
-            //mail.To.Add(toEmail);
-            //mail.Subject = subject;
-            //mail.Body = body;
-            //mail.IsBodyHtml = true;
-            //if (isReply)
-            //{
-            //    mail.ReplyTo = new MailAddress(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]));
-            //}
-            //SmtpServer.Port = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
-            //SmtpServer.UseDefaultCredentials = false;
-            //SmtpServer.Credentials = new System.Net.NetworkCredential(Convert.ToString(ConfigurationManager.AppSettings["EmailUserName"]), Convert.ToString(ConfigurationManager.AppSettings["EmailPassword"]));
-            //SmtpServer.EnableSsl = true;
-            //SmtpServer.Send(mail);
         }
     }
 }
