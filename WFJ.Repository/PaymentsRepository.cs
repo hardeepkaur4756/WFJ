5b4dca123eaa1739f3b0b742e1b26c8dfb04dce0
Developer <developer>
2021-06-23 19:16:40 +0530

    commit for approve payment

--- WFJ.Repository/PaymentsRepository.cs
+++ WFJ.Repository/PaymentsRepository.cs
@@ -52,13 +52,27 @@ namespace WFJ.Repository
 
         public IEnumerable<Payment> GetPaymentByApprovedAndXDays(int approved, int userId)
         {
-            if (userId > 0)
+            if(approved == 0)
             {
-                return _context.Payments.Where(x => x.approved == approved && x.Request != null && x.UserID == userId).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                if (userId > 0)
+                {
+                    return _context.Payments.Where(x => x.approved == 0 || x.approved == null && x.Request != null && x.UserID == userId).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                }
+                else
+                {
+                    return _context.Payments.Where(x => x.approved == 0 || x.approved == null && x.Request != null).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                }
             }
             else
             {
-                return _context.Payments.Where(x => x.approved == approved && x.Request != null).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                if (userId > 0)
+                {
+                    return _context.Payments.Where(x => x.approved == 1 && x.Request != null && x.UserID == userId).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                }
+                else
+                {
+                    return _context.Payments.Where(x => x.approved == 1 && x.Request != null).OrderByDescending(x => x.Request.RequestDate).Take(7);
+                }
             }
         }
     }
